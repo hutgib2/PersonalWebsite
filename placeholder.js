3a858f2259b201e14cddf66d37c82728184ccad6
Hugo Smith <hugowsmith1301@gmail.com>
2026-01-16 09:29:03 +0000

    added burp

--- placeholder.js
+++ placeholder.js
@@ -1,1 +1,33 @@
-console.log("Gill is OP")
+console.log("Neptune is Overpowered")
+var tutor = "Ronan"
+console.log(tutor)
+var tutor = "Eden"
+console.log(tutor)
+var num_1 = 68
+var num_2 = 21
+var num_3 = num_1*num_2
+console.log(num_3)
+const hugo_add = (add_1,add_2) => {
+    // console.log("i am here")
+    // console.log(`i have been told to add ${add_1}, and ${add_2}`)
+    return add_1+add_2
+}
+
+const answer = hugo_add(77,88)
+// conditional statements
+if (true) {
+    console.log(hugo_add(1,1))
+}
+
+if (false) {
+    console.log(hugo_add(100, 11))
+}
+
+// this function adds 2 numbers together
+
+const burp = () => {
+    const burp = "burp"
+    return burp
+}
+
+console.log(burp())
